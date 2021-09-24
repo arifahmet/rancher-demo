b3baa68eed56e5846678e492e6ffa64858e15d7c
Arif Ahmet <arifahmetbarbak@gmail.com>
2021-09-25 01:16:03 +0300

    rafactor show message

--- src/main/java/com/example/rancher_demo/web/controller/HelloController.java
+++ src/main/java/com/example/rancher_demo/web/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.rancher_demo.web.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,11 +8,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 @RequestMapping("api/v1/hello")
 public class HelloController {
-    @GetMapping
-    public ResponseEntity<String> hello(@RequestParam(required = false) String name) {
-        return ResponseEntity.ok("Hello " + (StringUtils.hasText(name) ? name: "world") + "!!");
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> hello(@RequestParam(required = false) String name) {
+
+        return ResponseEntity.ok("{\"Message\":\"Hello " + (StringUtils.hasText(name) ? name : "world") + "!\"" +
+                getInfo() + "}");
+    }
+
+    private String getInfo() {
+        String res = "";
+        try {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            res = ",\"Hostname\" : \"" + inetAddress.getHostName() + "\"" +
+                    ", \"HostAddress\" : \"" + inetAddress.getHostAddress() + "\"" +
+                    ", \"CanonicalHostName\" : \"" + inetAddress.getCanonicalHostName() + "\"" +
+                    ", \"HostAddress\" : \"" + inetAddress.getHostAddress() + "\"";
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return res;
     }
 }
